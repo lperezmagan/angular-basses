344dead6675bba3de4e9cebc8916a4ca7f3c83b7
Laura Perez Magan <lperezm@minsait.com>
2025-02-10 11:54:02 +0100

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h1>Hola Counterrr</h1>
+  <h3>Counter: {{counter}}</h3>
+
+  <button (click)="increaseBy(1)">+1</button>
+  <button (click)="resetCounter()">Reset</button>
+  <button (click)="reduceBy(1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+
+  public counter: number = 10;
+
+  public increaseBy( value: number): void {
+    this.counter += value;
+  }
+
+  public reduceBy( value: number): void {
+    this.counter -= value;
+  }
+
+  public resetCounter(): void {
+    this.counter = 10;
+  }
+
+}
---
+++ src/app/heroes/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+
+export class ListComponent {
+
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor',
+    'Laura'
+  ];
+
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+    console.log('deletedHero: ', this.deletedHero);
+  }
+
+}
